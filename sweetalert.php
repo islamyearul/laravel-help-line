bfbc82c9f33cf8ac1af289a4539085ef969fde42
Md Yearul Islam <72243025+islamyearul@users.noreply.github.com>
2022-07-20 12:37:47 +0600

    Update sweetalert.php
    new system

--- sweetalert.php
+++ sweetalert.php
@@ -33,3 +33,32 @@
             });
         });
     </script>
+
+
+<!-- New system Edited by Yearul -->
+{{-- Sweet Alert  --}}
+
+cdn
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <script type="text/javascript">
+ 
+        $('.show_confirm').click(function(event) {
+             var form =  $(this).closest("form");
+             var name = $(this).data("name");
+             event.preventDefault();
+             swal({
+                 title: `Are you sure you want to delete this record?`,
+                 text: "If you delete this, it will be gone forever.",
+                 icon: "warning",
+                 buttons: true,
+                 dangerMode: true,
+             })
+             .then((willDelete) => {
+               if (willDelete) {
+                 form.submit();
+               }
+             });
+         });
+     
+   </script>
