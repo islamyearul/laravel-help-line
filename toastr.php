8fb95880ffaa51d4e63df71f6e47a4176a5f2922
Md Yearul Islam <72243025+islamyearul@users.noreply.github.com>
2022-08-10 18:27:53 +0600

    Update toastr.php

--- toastr.php
+++ toastr.php
@@ -34,5 +34,14 @@
     </script>
 
 
+return redirect()->route('your route name')->with('message','Data added Successfully');
+
+return redirect()->route('your route name')->with('error','Data Deleted');
+
+return redirect()->route('your route name')->with('Warning','Are you sure you want to delete? ');
+
+return redirect()->route('your route name')->with('info','This is xyz information');
+
+
 
 
