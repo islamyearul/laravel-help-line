a0087ff4dd6df2ca6af03ce7c0e03446b75f9ba5
Md Yearul Islam <72243025+islamyearul@users.noreply.github.com>
2022-12-12 10:54:56 +0600

    Update data-table-update-option.php

--- data-table-update-option.php
+++ data-table-update-option.php
@@ -31,3 +31,63 @@ JS CDN and Script
             });
         });
     </script>
+================================
+Dynamic table data-table example
+
+ success: function(data) {
+                        // console.log(data.status)
+                        if (data.status == 'success') {
+                            console.log(data);
+                            wrapper.empty();
+                            $(wrapper).append("<table class='table table-bordered expandable-table' id='datatable-attendance'>\
+                                                <thead>\
+                                                    <tr>\
+                                                        <th>Employe Name</th>\
+                                                        <th>Attendance Date</th>\
+                                                        <th>Status</th>\
+                                                        <th>In Time</th>\
+                                                        <th>Out Time</th>\
+                                                        <th>Hour</th>\
+                                                        <th>Day</th>\
+                                                        <th>Note</th>\
+                                                    </tr>\
+                                                </thead>\
+                                                <tbody id=''></tbody></table>");
+                            $.each(data.data, function(key, value) {
+                                var html = '<tr>';
+                                html += '<td>' + value.employee.emp_name + '</td>';
+                                html += '<td>' + value.date + '</td>';
+                                html += '<td>' + value.status + '</td>';
+                                html += '<td>' + value.time_in + '</td>';
+                                html += '<td>' + value.time_out + '</td>';
+                                html += '<td>' + value.hours_worked + '</td>';
+                                html += '<td>' + value.week_day + '</td>';
+                                html += '<td>' + value.note + '</td>';
+                                html += '</tr>';
+                                $('#datatable-attendance tbody').append(html);
+                            });
+
+                        } else {
+                            wrapper.empty();
+                            $(wrapper).append("<h2 class='text-center'>No Data Found</h2>");
+                           
+                        }
+                        var table = $('#datatable-attendance').DataTable({
+                            "aaSorting": [],
+                            "ordering": false,
+                            dom: 'Bfrtip',
+                            lengthMenu: [
+                                [10, 25, 50, -1],
+                                ['10 rows', '25 rows', '50 rows', 'Show all']
+                            ],
+                            buttons: [
+                                'pageLength', 'copy', 'csv', 'excel', 'pdf',
+                                'print', 'colvis'
+                            ],
+                        })
+                        table.ajax.reload();
+                        $.fn.dataTable.ext.errMode = function(obj, param, err) {
+                            var tableId = obj.sTableId;
+                            console.log('Handling DataTable issue of Table ' + tableId);
+                        };
+                    }
