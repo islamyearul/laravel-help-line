46e245bce85bc3dd681b43d45cc18e58b4a83d23
Md Yearul Islam <72243025+islamyearul@users.noreply.github.com>
2023-02-08 16:28:31 +0600

    Update image_upload.php

--- image_upload.php
+++ image_upload.php
@@ -28,27 +28,7 @@ $oldFile = storage_path().$User->profile_photo_path;
                 unlink($Old_file_path); //delete from storage
              }
 
-   if ($request->file('avatar')) {
-            $avatar = $request->file('avatar');
-            $avatarName = time() . '.' . $avatar->getClientOriginalExtension();
-            $avatarName = str_replace(' ', '_', $avatarName);
-            $avatarPath = public_path('/images/');
-            if ($avatar->move($avatarPath, $avatarName)) {
-                if ($user->avatar) {
-                    $oldFile = public_path($user->avatar);
-                    if (file_exists($oldFile) && !unlink($oldFile)) {
-                        $user->avatar = '/images/' . $avatarName;
-                        $user->save();
-                        $notification = array(
-                            'message' => 'User Profile Updated Successfully, But Old Avatar Not Deleted',
-                            'alert-type' => 'warning',
-                        );
-                        return redirect()->route('userProfile')->with($notification);
-                    }
-                }               
-                $user->avatar = '/images/' . $avatarName;
-            }
-        }
+
 
 
 //Multiimage upload
