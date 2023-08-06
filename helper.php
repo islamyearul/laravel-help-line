28a24600e10d54d9c7e942672d72126f57b23144
Md Yearul Islam <72243025+islamyearul@users.noreply.github.com>
2023-08-06 23:44:53 +0600

    Update helper.php

--- helper.php
+++ helper.php
@@ -75,7 +75,17 @@
 
         return $data->except(auth()->id); # except whoever you wanted
     }
-=======================================                                      
+=======================================    
+                function randomPassword() {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); //turn the array into a string
+    }
 =======================================   
 =======================================                                     
 =======================================   
