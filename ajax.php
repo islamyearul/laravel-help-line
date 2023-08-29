38813a3d217d420d2a0bee4e4432b8720393ec55
Md Yearul Islam <72243025+islamyearul@users.noreply.github.com>
2023-08-29 16:37:29 +0600

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,4 +1,6 @@
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+var url = "{{ route('admin.get_parcel_history', ':id') }}".replace(':id', id);
+=========================
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <!-- header link -->
 
