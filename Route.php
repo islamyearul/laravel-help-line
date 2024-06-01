a0973975791cb130b95bd19fda4366fb113a38dc
Md Yearul Islam <72243025+islamyearul@users.noreply.github.com>
2024-06-01 12:46:45 +0600

    Update Route.php

--- Route.php
+++ Route.php
@@ -1,1 +1,15 @@
+//Clear Config cache:
+Route::get('/cc', function () {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('route:clear');
+    $exitCode = Artisan::call('view:clear');
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>All Config cleared</h1>';
+});
 
+// Storage Link
+
+Route::get('/link', function () {
+    $exitCode = Artisan::call('storage:link');
+    return '<h1>All Config cleared</h1>';
+});
